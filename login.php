eec15908bb4e960831cf849b2463101338828885
phillipgrummett1 <101611913+phillipgrummett1@users.noreply.github.com>
2022-04-20 00:54:11 -0400

    Add files via upload

--- login.php
+++ login.php
@@ -1,10 +1,14 @@
 <?php
-
+session_start();
 $user=$_POST['username']??null;
 $password=$_POST['password']??null;
 include 'includes/library.php';//Data base connection
 $errors = array();
 $pdo=connectDB();
+$_SESSION['user'] = $user;
+ $_SESSION['pswd'] = $password ??null;  //- to auto fill uhhashed password in update field? 
+
+
 
 if(isset($_POST['Forgot'])) //Redirects to forgot php page
 {
@@ -19,29 +23,27 @@ if(isset($_POST['login']))
   }
   else
   {
-    $query= "SELECT * FROM Userbase where username = ?";
+    $query= "SELECT username, passkey, email FROM userbase where username =?";
     $stmt=$pdo->prepare($query);
   $stmt->execute([$user]);
   $row=$stmt->fetch();
   if(!$row){ //Returns nothing
     $errors['login'] =true;
+    
   }
   else //There is a unique user name
   {
-    if(password_verify($pass,$row['password'])){
+    if(password_verify($password,$row['passkey'])){
         session_start();
-        $_SESSION['user']=$user;
+        $_SESSION['username']=$user;
         header("Location:main.php"); //Redirect to Homepage
       }
       else{
         $errors['login'] =true;
+        
       }
-
   }
-
-  }
-
-
+}
 }
 
 
@@ -56,32 +58,42 @@ if(isset($_POST['login']))
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="styles/master.css" />
     <title>Login</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
   </head>
 
+  <header>
+    <?php include "nav.php";?>
+  </header>
 
   <body>
+  <section id="loginform">
       <!-- Form box for login page -->
     <form id ="login" method="post">
     <div> <label for ="username">Username </label> 
     <input type="text" id="username" name="username" size="40"/>
+    
     </div>
     <div>
     <label for="password">Password:</label> 
     <input type="password" id="password" name="password" size="40" />
+    
     </div>
     </div>
-    <div id="login'">    
+    <div id="login1">    
     <button type="submit" name="login">Login</button>
     </div>
     </div>
-    <div id="login'">    
-    <button type="submit" name="Forgot">Forgot Login details</button>
+    <div id="login1">    
+    <button type="submit" name="Forgot">Forgot Password?</button>
     </div>
     <div>
-            <span class="error <?=!isset($errors['login']) ? 'hidden' : "";?>">Your username or password was invalid</span>
-            </div>
+      <span class="error <?=!isset($errors['login']) ? 'hidden' : "";?>">Your username or password was invalid</span>
+    </div>
     </form>
-
+</section >
 
 
   </body>
---
+++ nav.php
@@ -0,0 +1,9 @@
+<div class="scroll">
+<nav>
+    <h1>COIS 3420 - Final Project</h1>
+            <ul>
+                <li><a href="main.php">Home</a></li> 
+                <li><a href="login.php">Logout</a></li>
+            </ul>
+</nav>
+</div>
