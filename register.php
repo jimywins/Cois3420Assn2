1e9d72dba4d6024ba98f3d1067d22a68672be2d6
jimywins <jimynguyen89@hotmail.ca>
2022-04-21 18:50:30 -0400

    Update register.php

--- register.php
+++ register.php
@@ -1,9 +1,10 @@
 <?php
+
 $user=$_POST['username']??null;
-$email=$_POST['email']??null;
-$cemail=$_POST['Cemail']??null;
+$email=$_POST['Email']??null;
+$cemail=$_POST['CEmail']??null;
 $pass=$_POST['pass']??null;
-$cpass=$_POST['Cpass']??null;
+$cpass=$_POST['cpass']??null;
 include 'include/library.php';//Data base connection
 $errors = array();
 $pdo=connectDB();
@@ -12,47 +13,54 @@ $pdo=connectDB();
 if(isset($_POST['create'])){
 
 
-    if(!isset($user) || strlen($user) === 0){ //Cant check if there no username
+    if(!isset($user) || (strlen($user) == 0) ) { //Cant register if theres  no username
         $errors['user'] = true;
+        echo(1);
       }
       else{//They did set a username
-    $query= "SELECT * FROM Userbase where username = ?";
+    $query= "SELECT * FROM userbase where username = ?";
     $stmt=$pdo->prepare($query);
     $stmt->execute([$user]);
     $row=$stmt->fetch();
     if($row){//There is another username
         $errors['user'] =true;
+        echo(2);
+        header("Location:login.php");
     }
     else //Is unique user
     {
-     if( !filter_var($email, FILTER_VALIDATE_EMAIL)&&!filter_var($cemail, FILTER_VALIDATE_EMAIL)){ //Check if both emails entered are valid
+     if( !filter_var($email, FILTER_VALIDATE_EMAIL) && !filter_var($cemail, FILTER_VALIDATE_EMAIL)){ //Check if both emails entered are valid
         if(!strcmp($email,$cemail)){//If email is valid check if both emails are the same
         
             $errors['email'] =true;
+            echo(3);
 
         }
 
         }
-    if(!strcmp($pass,$cpass)){//Check if both enter password are the same
-    $errors['pass']=true;
+    if($pass !== $cpass){//Check if both enter password are the same
+    $errors['pass']=true; 
+    
     }
     }
 
     if(count($errors)===0)//No Errors on creating account
     {
-        $query="INSERT INTO `jimynguyen`.`Userbase` (`username`, `password`, `email`) VALUES(?,?,?)";
+        session_start();
+        $_SESSION['username'] =$user;
+        $query="INSERT INTO userbase  VALUES(?,?,?)";
         $password= password_hash($pass,PASSWORD_DEFAULT);
         $stmt=$pdo->prepare($query);
-        $stmt->execute([$user],[$password],[$email]);
+        $stmt->execute([$user, $password, $email]);
 
         //Code to send email
         $to=$email;
         $subject="Account Creation";
         $message="Thank you for creating your account you may now send an email";
 
-        mail($email,$subject,$message);//Send email
-
-
+        header("Location:main.php"); //redirect to main page 
+exit();
+   
     }
 
     }
@@ -69,16 +77,25 @@ if(isset($_POST['create'])){
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="styles/master.css" />
-    <title>Login</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com/%22%3E">
-    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
+    <script defer src="scripts/master.js"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+    <title>Register</title>
   </head>
 
 
+
+
+
   <body>
+  <header>
+    <?php include "nav.php";?>
+  </header>
+  <div id="register">
       <!-- Form box for login page -->
-    <form id ="login" method="post">
+      <h1>Register</h1>
+    <form id ="login" method="post" action="<?php htmlentities($_SERVER['PHP_SELF'])?>" enctype="multipart/form-data">
     <div> <label for ="username">Enter your Username for login </label> 
     <input type="text" id="username" name="username" size="40"/>
     </div>
@@ -88,7 +105,7 @@ if(isset($_POST['create'])){
     </div>
     <div>
     <label for="password">Confirm your password </label> 
-    <input type="password" id="Cpass" name="Cpass" size="40" />
+    <input type="password" id="Cpass" name="cpass" size="40" />
     </div>
     <div>
     <label for="Email">Enter your email address </label> 
@@ -104,11 +121,10 @@ if(isset($_POST['create'])){
     </div>
     </div>
 
-    <div>
-            <span class="error <?=!isset($errors['login']) ? 'hidden' : "";?>">Your username or password was invalid</span>
-            </div>
-    </form>
+    
 
 
 
   </body>
+
+  </html>
